4d21c239e41114077ff0464f9657d2a3c5add8f0
ajay1706 <ajay97456@gmail.com>
2019-09-27 14:51:35 +0530

    adding InstaBody & InstaList

--- instagram_clone/lib/insta_list.dart
+++ instagram_clone/lib/insta_list.dart
@@ -11,7 +11,51 @@ class InstaList extends StatelessWidget {
       ? new SizedBox(
         child: InstaStories(),
         height: deviceSize.height *0.15,
-      ):Column(),
+      ):Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.fromLTRB(16, 16, 8, 16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Container(
+                      height: 40,
+                      width: 40,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(  
+                          fit: BoxFit.fill,
+                        image: NetworkImage("https://avatars3.githubusercontent.com/u/20722807?s=460&v=4"),
+
+                        )
+
+                        
+                      ),
+                    ),
+                
+                SizedBox(
+                  width: 15,
+                ),
+                Text("droid_aj",
+                style: TextStyle(fontWeight: FontWeight.bold),),
+                  ],
+                ),
+                IconButton(icon: Icon(Icons.more_vert),
+                onPressed: (){},)
+              ],
+            ),
+          ),
+          Flexible(
+            fit: FlexFit.loose,
+            child: Image.network("https://avatars3.githubusercontent.com/u/20722807?s=460&v=4"),
+          )
+        ],
+      ),
       
     );
   }
