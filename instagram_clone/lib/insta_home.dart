8831049bdc9d6c640d8785cd3113531b6ff776dd
ajay1706 <ajay97456@gmail.com>
2019-09-27 14:12:53 +0530

    adding  bottom navigation bar

---
+++ instagram_clone/lib/insta_home.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class InstaHome extends StatelessWidget {
+
+final topBar = AppBar(
+backgroundColor: Color(0xfff8faf8),
+centerTitle: true,
+elevation: 1.0,
+leading: Icon(Icons.camera_alt),
+title: SizedBox(
+  height: 35.0,
+  child: Image.asset("assets/images/insta_logo.png")),
+  actions: <Widget>[
+    Padding( 
+      padding: EdgeInsets.only(right: 14.0),
+      child: Icon(Icons.send),
+    )
+  ],
+);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: topBar,
+      body: Container(),
+      bottomNavigationBar: Container(
+        color: Colors.white,
+        height: 60.0,
+        alignment: Alignment.center,
+        child: BottomAppBar(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              IconButton(
+                icon: Icon(
+                  Icons.home
+                ),
+                onPressed: (){},
+              ),
+                 IconButton(
+                icon: Icon(
+                  Icons.search
+                ),
+                onPressed: (){},
+              ),
+                 IconButton(
+                icon: Icon(
+                  Icons.add_box
+                ),
+                onPressed: (){},
+              ),
+                 IconButton(
+                icon: Icon(
+                  Icons.favorite
+                ),
+                onPressed: (){},
+              ),
+                 IconButton(
+                icon: Icon(
+                  Icons.account_box
+                ),
+                onPressed: (){},
+              )
+            ],
+          ),
+        ),
+      ),
+      
+    );
+  }
+}
--- instagram_clone/lib/main.dart
+++ instagram_clone/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './insta_home.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,7 +7,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      title: "Instagram",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(  
+        primaryColor: Colors.black,
+        primaryIconTheme: IconThemeData(color: Colors.black),
+        primaryTextTheme: TextTheme(
+          title: TextStyle(color: Colors.black, fontFamily: "Aveny")),
+        textTheme: TextTheme(title: TextStyle(color: Colors.black))
+      ),
+            home: InstaHome(),
+
       
     );
   }
